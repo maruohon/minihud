08b5d799689d39a4d2bb11aa391608690472a634
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2017-04-20 14:35:24 +0300

    Update Forge and mappings

--- src/main/java/fi/dy/masa/itemscroller/config/ItemScrollerGuiFactory.java
+++ src/main/java/fi/dy/masa/itemscroller/config/ItemScrollerGuiFactory.java
@@ -1,10 +1,8 @@
 package fi.dy.masa.itemscroller.config;
 
 import java.util.Set;
-
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
-
 import net.minecraftforge.fml.client.IModGuiFactory;
 
 public class ItemScrollerGuiFactory implements IModGuiFactory
@@ -18,6 +16,18 @@ public class ItemScrollerGuiFactory implements IModGuiFactory
     public Class<? extends GuiScreen> mainConfigGuiClass()
     {
         return ItemScrollerConfigGui.class;
+    }
+
+    @Override
+    public boolean hasConfigGui()
+    {
+        return true;
+    }
+
+    @Override
+    public GuiScreen createConfigGui(GuiScreen parentScreen)
+    {
+        return new ItemScrollerConfigGui(parentScreen);
     }
 
     @Override
