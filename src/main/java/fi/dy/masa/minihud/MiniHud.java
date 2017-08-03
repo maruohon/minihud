d46e2b15850c57783630967449253fd843b18729
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2017-08-03 14:35:39 +0300

    Sign the JAR

--- src/main/java/fi/dy/masa/minihud/MiniHud.java
+++ src/main/java/fi/dy/masa/minihud/MiniHud.java
@@ -3,12 +3,13 @@ package fi.dy.masa.minihud;
 import org.apache.logging.log4j.Logger;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import fi.dy.masa.minihud.config.Configs;
 import fi.dy.masa.minihud.event.RenderEventHandler;
 import fi.dy.masa.minihud.proxy.CommonProxy;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION,
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION, certificateFingerprint = Reference.FINGERPRINT,
     guiFactory = "fi.dy.masa.minihud.config.MiniHudGuiFactory",
     updateJSON = "https://raw.githubusercontent.com/maruohon/minihud/master/update.json",
     clientSideOnly=true, acceptedMinecraftVersions = "[1.11,1.11.2]")
@@ -33,5 +34,24 @@ public class MiniHud
 
         proxy.registerKeyBindings();
         proxy.registerEventHandlers();
+    }
+
+    @Mod.EventHandler
+    public void onFingerPrintViolation(FMLFingerprintViolationEvent event)
+    {
+        // Not running in a dev environment
+        if (event.isDirectory() == false)
+        {
+            logger.warn("*********************************************************************************************");
+            logger.warn("*****                                    WARNING                                        *****");
+            logger.warn("*****                                                                                   *****");
+            logger.warn("*****   The signature of the mod file '{}' does not match the expected fingerprint!     *****", event.getSource().getName());
+            logger.warn("*****   This might mean that the mod file has been tampered with!                       *****");
+            logger.warn("*****   If you did not download the mod {} directly from Curse/CurseForge,       *****", Reference.MOD_NAME);
+            logger.warn("*****   or using one of the well known launchers, and you did not                       *****");
+            logger.warn("*****   modify the mod file at all yourself, then it's possible,                        *****");
+            logger.warn("*****   that it may contain malware or other unwanted things!                           *****");
+            logger.warn("*********************************************************************************************");
+        }
     }
 }
