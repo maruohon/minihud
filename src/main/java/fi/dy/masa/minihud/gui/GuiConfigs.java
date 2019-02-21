16bde25880fe49a6979fde78b099d7d2fb7f4771
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2019-02-21 17:38:08 +0200

    Update to GUI clean-up in malilib

--- src/main/java/fi/dy/masa/minihud/gui/GuiConfigs.java
+++ src/main/java/fi/dy/masa/minihud/gui/GuiConfigs.java
@@ -35,32 +35,22 @@ public class GuiConfigs extends GuiConfigsBase
         super.initGui();
         this.clearOptions();
 
-        this.id = 0;
         int x = 10;
         int y = 26;
 
         for (ConfigGuiTab tab : ConfigGuiTab.values())
         {
-            x += this.createButton(x, y, -1, tab) + 4;
+            x += this.createButton(x, y, -1, tab);
         }
     }
 
     private int createButton(int x, int y, int width, ConfigGuiTab tab)
     {
-        ButtonListener listener = new ButtonListener(tab, this);
-        boolean enabled = GuiConfigs.tab != tab;
-        String label = tab.getDisplayName();
+        ButtonGeneric button = new ButtonGeneric(x, y, width, 20, tab.getDisplayName());
+        button.enabled = GuiConfigs.tab != tab;
+        this.addButton(button, new ButtonListenerConfigTabs(tab, this));
 
-        if (width < 0)
-        {
-            width = this.mc.fontRenderer.getStringWidth(label) + 10;
-        }
-
-        ButtonGeneric button = new ButtonGeneric(this.id++, x, y, width, 20, label);
-        button.enabled = enabled;
-        this.addButton(button, listener);
-
-        return width;
+        return button.getButtonWidth() + 2;
     }
 
     @Override
