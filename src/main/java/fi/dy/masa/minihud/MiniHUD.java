3489f921c55bfd04c7aa57f48124db24f50afbdc
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2020-04-27 22:38:15 +0300

    Add an option for debug messages, use it for the structure data channels

--- src/main/java/fi/dy/masa/minihud/MiniHUD.java
+++ src/main/java/fi/dy/masa/minihud/MiniHUD.java
@@ -3,6 +3,7 @@ package fi.dy.masa.minihud;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import fi.dy.masa.malilib.event.InitializationHandler;
+import fi.dy.masa.minihud.config.Configs;
 import net.fabricmc.api.ModInitializer;
 
 public class MiniHUD implements ModInitializer
@@ -14,5 +15,13 @@ public class MiniHUD implements ModInitializer
     public void onInitialize()
     {
         InitializationHandler.getInstance().registerInitializationHandler(new InitHandler());
+    }
+
+    public static void printDebug(String key, Object... args)
+    {
+        if (Configs.Generic.DEBUG_MESSAGES.getBooleanValue())
+        {
+            logger.info(key, args);
+        }
     }
 }
