1ec32ca42e78044fbc4b99a62ac49d0e2b7a6ac6
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2020-05-07 00:01:01 +0300

    Update to MC 1.16 snapshot 20w19a

--- src/main/java/fi/dy/masa/itemscroller/event/RenderEventHandler.java
+++ src/main/java/fi/dy/masa/itemscroller/event/RenderEventHandler.java
@@ -299,7 +299,7 @@ public class RenderEventHandler
             stack = stack.copy();
             InventoryUtils.setStackSize(stack, 1);
             this.mc.getItemRenderer().zOffset += 100;
-            this.mc.getItemRenderer().renderGuiItem(this.mc.player, stack, x, y);
+            this.mc.getItemRenderer().renderGuiItem(stack, x, y);
             this.mc.getItemRenderer().zOffset -= 100;
         }
 
