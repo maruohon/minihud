d565d39c68bdcd3ed1e1cf2007491e03d9659f34
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2020-05-27 02:56:00 +0300

    Fix the structure bounding boxes not working in single player

--- src/main/java/fi/dy/masa/minihud/util/StructureType.java
+++ src/main/java/fi/dy/masa/minihud/util/StructureType.java
@@ -14,27 +14,27 @@ import fi.dy.masa.minihud.config.StructureToggle;
 
 public enum StructureType
 {
-    BURIED_TREASURE     (StructureToggle.OVERLAY_STRUCTURE_BURIED_TREASURE,     "Buried_Treasure",      DimensionType.OVERWORLD_REGISTRY_KEY),
-    DESERT_PYRAMID      (StructureToggle.OVERLAY_STRUCTURE_DESERT_PYRAMID,      "Desert_Pyramid",       DimensionType.OVERWORLD_REGISTRY_KEY),
-    IGLOO               (StructureToggle.OVERLAY_STRUCTURE_IGLOO,               "Igloo",                DimensionType.OVERWORLD_REGISTRY_KEY),
-    JUNGLE_TEMPLE       (StructureToggle.OVERLAY_STRUCTURE_JUNGLE_TEMPLE,       "Jungle_Pyramid",       DimensionType.OVERWORLD_REGISTRY_KEY),
-    MANSION             (StructureToggle.OVERLAY_STRUCTURE_MANSION,             "Mansion",              DimensionType.OVERWORLD_REGISTRY_KEY),
-    MINESHAFT           (StructureToggle.OVERLAY_STRUCTURE_MINESHAFT,           "Mineshaft",            DimensionType.OVERWORLD_REGISTRY_KEY),
-    OCEAN_MONUMENT      (StructureToggle.OVERLAY_STRUCTURE_OCEAN_MONUMENT,      "Monument",             DimensionType.OVERWORLD_REGISTRY_KEY),
-    OCEAN_RUIN          (StructureToggle.OVERLAY_STRUCTURE_OCEAN_RUIN,          "Ocean_Ruin",           DimensionType.OVERWORLD_REGISTRY_KEY),
-    PILLAGER_OUTPOST    (StructureToggle.OVERLAY_STRUCTURE_PILLAGER_OUTPOST,    "Pillager_Outpost",     DimensionType.OVERWORLD_REGISTRY_KEY),
-    SHIPWRECK           (StructureToggle.OVERLAY_STRUCTURE_SHIPWRECK,           "Shipwreck",            DimensionType.OVERWORLD_REGISTRY_KEY),
-    STRONGHOLD          (StructureToggle.OVERLAY_STRUCTURE_STRONGHOLD,          "Stronghold",           DimensionType.OVERWORLD_REGISTRY_KEY),
-    VILLAGE             (StructureToggle.OVERLAY_STRUCTURE_VILLAGE,             "Village",              DimensionType.OVERWORLD_REGISTRY_KEY),
-    WITCH_HUT           (StructureToggle.OVERLAY_STRUCTURE_WITCH_HUT,           "Swamp_Hut",            DimensionType.OVERWORLD_REGISTRY_KEY),
+    BURIED_TREASURE     (StructureToggle.OVERLAY_STRUCTURE_BURIED_TREASURE,     "buried_treasure",      DimensionType.OVERWORLD_REGISTRY_KEY),
+    DESERT_PYRAMID      (StructureToggle.OVERLAY_STRUCTURE_DESERT_PYRAMID,      "desert_pyramid",       DimensionType.OVERWORLD_REGISTRY_KEY),
+    IGLOO               (StructureToggle.OVERLAY_STRUCTURE_IGLOO,               "igloo",                DimensionType.OVERWORLD_REGISTRY_KEY),
+    JUNGLE_TEMPLE       (StructureToggle.OVERLAY_STRUCTURE_JUNGLE_TEMPLE,       "jungle_pyramid",       DimensionType.OVERWORLD_REGISTRY_KEY),
+    MANSION             (StructureToggle.OVERLAY_STRUCTURE_MANSION,             "mansion",              DimensionType.OVERWORLD_REGISTRY_KEY),
+    MINESHAFT           (StructureToggle.OVERLAY_STRUCTURE_MINESHAFT,           "mineshaft",            DimensionType.OVERWORLD_REGISTRY_KEY),
+    OCEAN_MONUMENT      (StructureToggle.OVERLAY_STRUCTURE_OCEAN_MONUMENT,      "monument",             DimensionType.OVERWORLD_REGISTRY_KEY),
+    OCEAN_RUIN          (StructureToggle.OVERLAY_STRUCTURE_OCEAN_RUIN,          "ocean_ruin",           DimensionType.OVERWORLD_REGISTRY_KEY),
+    PILLAGER_OUTPOST    (StructureToggle.OVERLAY_STRUCTURE_PILLAGER_OUTPOST,    "pillager_outpost",     DimensionType.OVERWORLD_REGISTRY_KEY),
+    SHIPWRECK           (StructureToggle.OVERLAY_STRUCTURE_SHIPWRECK,           "shipwreck",            DimensionType.OVERWORLD_REGISTRY_KEY),
+    STRONGHOLD          (StructureToggle.OVERLAY_STRUCTURE_STRONGHOLD,          "stronghold",           DimensionType.OVERWORLD_REGISTRY_KEY),
+    VILLAGE             (StructureToggle.OVERLAY_STRUCTURE_VILLAGE,             "village",              DimensionType.OVERWORLD_REGISTRY_KEY),
+    WITCH_HUT           (StructureToggle.OVERLAY_STRUCTURE_WITCH_HUT,           "swamp_hut",            DimensionType.OVERWORLD_REGISTRY_KEY),
 
-    RUINED_PORTAL       (StructureToggle.OVERLAY_STRUCTURE_RUINED_PORTAL,       "Ruined_Portal",        DimensionType.OVERWORLD_REGISTRY_KEY, DimensionType.THE_NETHER_REGISTRY_KEY),
+    RUINED_PORTAL       (StructureToggle.OVERLAY_STRUCTURE_RUINED_PORTAL,       "ruined_portal",        DimensionType.OVERWORLD_REGISTRY_KEY, DimensionType.THE_NETHER_REGISTRY_KEY),
 
-    BASTION_REMNANT     (StructureToggle.OVERLAY_STRUCTURE_BASTION_REMNANT,     "Bastion_Remnant",      DimensionType.THE_NETHER_REGISTRY_KEY),
-    NETHER_FOSSIL       (StructureToggle.OVERLAY_STRUCTURE_NETHER_FOSSIL,       "Nether_Fossil",        DimensionType.THE_NETHER_REGISTRY_KEY),
-    NETHER_FORTRESS     (StructureToggle.OVERLAY_STRUCTURE_NETHER_FORTRESS,     "Fortress",             DimensionType.THE_NETHER_REGISTRY_KEY),
+    BASTION_REMNANT     (StructureToggle.OVERLAY_STRUCTURE_BASTION_REMNANT,     "bastion_remnant",      DimensionType.THE_NETHER_REGISTRY_KEY),
+    NETHER_FOSSIL       (StructureToggle.OVERLAY_STRUCTURE_NETHER_FOSSIL,       "nether_fossil",        DimensionType.THE_NETHER_REGISTRY_KEY),
+    NETHER_FORTRESS     (StructureToggle.OVERLAY_STRUCTURE_NETHER_FORTRESS,     "fortress",             DimensionType.THE_NETHER_REGISTRY_KEY),
 
-    END_CITY            (StructureToggle.OVERLAY_STRUCTURE_END_CITY,            "EndCity",              DimensionType.THE_END_REGISTRY_KEY);
+    END_CITY            (StructureToggle.OVERLAY_STRUCTURE_END_CITY,            "endcity",              DimensionType.THE_END_REGISTRY_KEY);
 
     public static final ImmutableList<StructureType> VALUES;
     private static final HashMap<String, StructureType> ID_TO_TYPE = new HashMap<>();
