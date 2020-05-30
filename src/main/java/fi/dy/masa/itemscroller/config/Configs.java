maruohon/itemscroller

Implement click packet rate limiting, by buffering the actual click packets

src/main/java/fi/dy/masa/itemscroller/config/Configs.java
@@ -7,6 +7,9 @@ import com.google.common.collect.ImmutableList;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import net.minecraft.client.gui.screen.ingame.CraftingTableScreen;
+import net.minecraft.client.gui.screen.ingame.InventoryScreen;
+import net.minecraft.container.CraftingResultSlot;
 import fi.dy.masa.itemscroller.Reference;
 import fi.dy.masa.itemscroller.recipes.CraftingHandler;
 import fi.dy.masa.itemscroller.recipes.CraftingHandler.SlotRange;
@@ -14,11 +17,9 @@ import fi.dy.masa.malilib.config.ConfigUtils;
 import fi.dy.masa.malilib.config.IConfigHandler;
 import fi.dy.masa.malilib.config.IConfigValue;
 import fi.dy.masa.malilib.config.options.ConfigBoolean;
+import fi.dy.masa.malilib.config.options.ConfigInteger;
 import fi.dy.masa.malilib.util.FileUtils;
 import fi.dy.masa.malilib.util.JsonUtils;
-import net.minecraft.client.gui.screen.ingame.CraftingTableScreen;
-import net.minecraft.client.gui.screen.ingame.InventoryScreen;
-import net.minecraft.container.CraftingResultSlot;
 
 public class Configs implements IConfigHandler
 {
@@ -29,8 +30,10 @@ public class Configs implements IConfigHandler
         public static final ConfigBoolean CARPET_CTRL_Q_CRAFTING                = new ConfigBoolean("carpetCtrlQCraftingEnabledOnServer",   false, "Set to true if the server is running the Carpet mod,\nand has the ctrlQCrafting option enabled.\nThis just changes which method Item Scroller uses\nfor the Drop key + Shift + Right click crafting.");
         public static final ConfigBoolean CLIENT_CRAFTING_FIX                   = new ConfigBoolean("clientCraftingFixOn1.12",              true, "Enable updating the crafting recipe output directly on the client side.\nThis fixes the quick/mass crafting and right-click-to-craft-a-stack\nfeatures othrwise being broken in 1.12.");
         public static final ConfigBoolean CRAFTING_RENDER_RECIPE_ITEMS          = new ConfigBoolean("craftingRenderRecipeItems",            true, "If enabled, then the recipe items are also rendered\nin the crafting recipe view.");
+        public static final ConfigInteger PACKET_RATE_LIMIT                     = new ConfigInteger("packetRateLimit",                      4, 1, 1024, "The limit of sent emulated slot click packets per game tick,\nif 'rateLimitClickPackets' is enabled");
         public static final ConfigBoolean SCROLL_CRAFT_STORE_RECIPES_TO_FILE    = new ConfigBoolean("craftingRecipesSaveToFile",            true, "If enabled, then the crafting features recipes are saved to a file\ninside minecraft/itemscroller/recipes_worldorservername.nbt.\nThis makes the recipes persistent across game restarts.");
         public static final ConfigBoolean SCROLL_CRAFT_RECIPE_FILE_GLOBAL       = new ConfigBoolean("craftingRecipesSaveFileIsGlobal",      false, "If true, then the recipe file is global, instead\n of being saved per-world or server");
+        public static final ConfigBoolean RATE_LIMIT_CLICK_PACKETS              = new ConfigBoolean("rateLimitClickPackets",                false, "This is meant for compatibility with Spigot servers and similar,\nwhich apply rate limits to packets from the client.\nThis queues up the emulated slot click packets and sends\nthem rate limited over time. The limit per game tick can be set in 'packetRateLimit´.");
         public static final ConfigBoolean REVERSE_SCROLL_DIRECTION_SINGLE       = new ConfigBoolean("reverseScrollDirectionSingle",         false, "Reverse the scrolling direction for single item mode.");
         public static final ConfigBoolean REVERSE_SCROLL_DIRECTION_STACKS       = new ConfigBoolean("reverseScrollDirectionStacks",         false, "Reverse the scrolling direction for full stacks mode.");
         public static final ConfigBoolean SLOT_POSITION_AWARE_SCROLL_DIRECTION  = new ConfigBoolean("useSlotPositionAwareScrollDirection",  false, "When enabled, the item movement direction depends\non the slots' y-position on screen. Might be derpy with more\ncomplex inventories, use with caution!");
@@ -40,6 +43,8 @@ public class Configs implements IConfigHandler
                 CARPET_CTRL_Q_CRAFTING,
                 CLIENT_CRAFTING_FIX,
                 CRAFTING_RENDER_RECIPE_ITEMS,
+                PACKET_RATE_LIMIT,
+                RATE_LIMIT_CLICK_PACKETS,
                 SCROLL_CRAFT_STORE_RECIPES_TO_FILE,
                 SCROLL_CRAFT_RECIPE_FILE_GLOBAL,
                 REVERSE_SCROLL_DIRECTION_SINGLE,

src/main/java/fi/dy/masa/itemscroller/event/KeybindCallbacks.java
@@ -15,6 +15,7 @@ import fi.dy.masa.itemscroller.recipes.CraftingHandler;
 import fi.dy.masa.itemscroller.recipes.RecipePattern;
 import fi.dy.masa.itemscroller.recipes.RecipeStorage;
 import fi.dy.masa.itemscroller.util.AccessorUtils;
+import fi.dy.masa.itemscroller.util.ClickPacketBuffer;
 import fi.dy.masa.itemscroller.util.InputUtils;
 import fi.dy.masa.itemscroller.util.InventoryUtils;
 import fi.dy.masa.itemscroller.util.MoveAction;
@@ -56,6 +57,20 @@ public class KeybindCallbacks implements IHotkeyCallback, IClientTickHandler
 
     @Override
     public boolean onKeyAction(KeyAction action, IKeybind key)
+    {
+        if (Configs.Generic.RATE_LIMIT_CLICK_PACKETS.getBooleanValue())
+        {
+            ClickPacketBuffer.setShouldBufferClickPackets(true);
+        }
+
+        boolean cancel = this.onKeyActionImpl(action, key);
+
+        ClickPacketBuffer.setShouldBufferClickPackets(false);
+
+        return cancel;
+    }
+
+    private boolean onKeyActionImpl(KeyAction action, IKeybind key)
     {
         MinecraftClient mc = MinecraftClient.getInstance();
 
@@ -167,10 +182,19 @@ public class KeybindCallbacks implements IHotkeyCallback, IClientTickHandler
     @Override
     public void onClientTick(MinecraftClient mc)
     {
-        if (this.disabled == false &&
-            mc != null &&
-            mc.player != null &&
-            GuiUtils.getCurrentScreen() instanceof ContainerScreen &&
+        if (this.disabled || mc.player == null)
+        {
+            return;
+        }
+
+        ClickPacketBuffer.sendBufferedPackets(Configs.Generic.PACKET_RATE_LIMIT.getIntegerValue());
+
+        if (ClickPacketBuffer.shouldCancelWindowClicks())
+        {
+            return;
+        }
+
+        if (GuiUtils.getCurrentScreen() instanceof ContainerScreen &&
             (GuiUtils.getCurrentScreen() instanceof CreativeInventoryScreen) == false &&
             Configs.GUI_BLACKLIST.contains(GuiUtils.getCurrentScreen().getClass().getName()) == false &&
             Hotkeys.KEY_MASS_CRAFT.getKeybind().isKeybindHeld())
@@ -181,6 +205,11 @@ public class KeybindCallbacks implements IHotkeyCallback, IClientTickHandler
 
             if (outputSlot != null)
             {
+                if (Configs.Generic.RATE_LIMIT_CLICK_PACKETS.getBooleanValue())
+                {
+                    ClickPacketBuffer.setShouldBufferClickPackets(true);
+                }
+
                 RecipePattern recipe = RecipeStorage.getInstance().getSelectedRecipe();
 
                 InventoryUtils.tryClearCursor(gui, mc);
@@ -205,6 +234,8 @@ public class KeybindCallbacks implements IHotkeyCallback, IClientTickHandler
                     InventoryUtils.throwAllNonRecipeItemsToGround(recipe, gui);
                     InventoryUtils.tryMoveItemsToFirstCraftingGrid(recipe, gui, true);
                 }
+
+                ClickPacketBuffer.setShouldBufferClickPackets(false);
             }
         }
     }

maruohon/minihud

src/main/java/fi/dy/masa/itemscroller/config/Configs.java
@@ -7,6 +7,9 @@ import com.google.common.collect.ImmutableList;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import net.minecraft.client.gui.screen.ingame.CraftingScreen;
+import net.minecraft.client.gui.screen.ingame.InventoryScreen;
+import net.minecraft.screen.slot.CraftingResultSlot;
 import fi.dy.masa.itemscroller.Reference;
 import fi.dy.masa.itemscroller.recipes.CraftingHandler;
 import fi.dy.masa.itemscroller.recipes.CraftingHandler.SlotRange;
@@ -14,11 +17,9 @@ import fi.dy.masa.malilib.config.ConfigUtils;
 import fi.dy.masa.malilib.config.IConfigHandler;
 import fi.dy.masa.malilib.config.IConfigValue;
 import fi.dy.masa.malilib.config.options.ConfigBoolean;
+import fi.dy.masa.malilib.config.options.ConfigInteger;
 import fi.dy.masa.malilib.util.FileUtils;
 import fi.dy.masa.malilib.util.JsonUtils;
-import net.minecraft.client.gui.screen.ingame.CraftingScreen;
-import net.minecraft.client.gui.screen.ingame.InventoryScreen;
-import net.minecraft.screen.slot.CraftingResultSlot;
 
 public class Configs implements IConfigHandler
 {
@@ -29,8 +30,10 @@ public class Configs implements IConfigHandler
         public static final ConfigBoolean CARPET_CTRL_Q_CRAFTING                = new ConfigBoolean("carpetCtrlQCraftingEnabledOnServer",   false, "Set to true if the server is running the Carpet mod,\nand has the ctrlQCrafting option enabled.\nThis just changes which method Item Scroller uses\nfor the Drop key + Shift + Right click crafting.");
         public static final ConfigBoolean CLIENT_CRAFTING_FIX                   = new ConfigBoolean("clientCraftingFixOn1.12",              true, "Enable updating the crafting recipe output directly on the client side.\nThis fixes the quick/mass crafting and right-click-to-craft-a-stack\nfeatures othrwise being broken in 1.12.");
         public static final ConfigBoolean CRAFTING_RENDER_RECIPE_ITEMS          = new ConfigBoolean("craftingRenderRecipeItems",            true, "If enabled, then the recipe items are also rendered\nin the crafting recipe view.");
+        public static final ConfigInteger PACKET_RATE_LIMIT                     = new ConfigInteger("packetRateLimit",                      4, 1, 1024, "The limit of sent emulated slot click packets per game tick,\nif 'rateLimitClickPackets' is enabled");
         public static final ConfigBoolean SCROLL_CRAFT_STORE_RECIPES_TO_FILE    = new ConfigBoolean("craftingRecipesSaveToFile",            true, "If enabled, then the crafting features recipes are saved to a file\ninside minecraft/itemscroller/recipes_worldorservername.nbt.\nThis makes the recipes persistent across game restarts.");
         public static final ConfigBoolean SCROLL_CRAFT_RECIPE_FILE_GLOBAL       = new ConfigBoolean("craftingRecipesSaveFileIsGlobal",      false, "If true, then the recipe file is global, instead\n of being saved per-world or server");
+        public static final ConfigBoolean RATE_LIMIT_CLICK_PACKETS              = new ConfigBoolean("rateLimitClickPackets",                false, "This is meant for compatibility with Spigot servers and similar,\nwhich apply rate limits to packets from the client.\nThis queues up the emulated slot click packets and sends\nthem rate limited over time. The limit per game tick can be set in 'packetRateLimit´.");
         public static final ConfigBoolean REVERSE_SCROLL_DIRECTION_SINGLE       = new ConfigBoolean("reverseScrollDirectionSingle",         false, "Reverse the scrolling direction for single item mode.");
         public static final ConfigBoolean REVERSE_SCROLL_DIRECTION_STACKS       = new ConfigBoolean("reverseScrollDirectionStacks",         false, "Reverse the scrolling direction for full stacks mode.");
         public static final ConfigBoolean SLOT_POSITION_AWARE_SCROLL_DIRECTION  = new ConfigBoolean("useSlotPositionAwareScrollDirection",  false, "When enabled, the item movement direction depends\non the slots' y-position on screen. Might be derpy with more\ncomplex inventories, use with caution!");
@@ -40,6 +43,8 @@ public class Configs implements IConfigHandler
                 CARPET_CTRL_Q_CRAFTING,
                 CLIENT_CRAFTING_FIX,
                 CRAFTING_RENDER_RECIPE_ITEMS,
+                PACKET_RATE_LIMIT,
+                RATE_LIMIT_CLICK_PACKETS,
                 SCROLL_CRAFT_STORE_RECIPES_TO_FILE,
                 SCROLL_CRAFT_RECIPE_FILE_GLOBAL,
                 REVERSE_SCROLL_DIRECTION_SINGLE,

src/main/java/fi/dy/masa/itemscroller/event/KeybindCallbacks.java
@@ -15,6 +15,7 @@ import fi.dy.masa.itemscroller.recipes.CraftingHandler;
 import fi.dy.masa.itemscroller.recipes.RecipePattern;
 import fi.dy.masa.itemscroller.recipes.RecipeStorage;
 import fi.dy.masa.itemscroller.util.AccessorUtils;
+import fi.dy.masa.itemscroller.util.ClickPacketBuffer;
 import fi.dy.masa.itemscroller.util.InputUtils;
 import fi.dy.masa.itemscroller.util.InventoryUtils;
 import fi.dy.masa.itemscroller.util.MoveAction;
@@ -56,6 +57,20 @@ public class KeybindCallbacks implements IHotkeyCallback, IClientTickHandler
 
     @Override
     public boolean onKeyAction(KeyAction action, IKeybind key)
+    {
+        if (Configs.Generic.RATE_LIMIT_CLICK_PACKETS.getBooleanValue())
+        {
+            ClickPacketBuffer.setShouldBufferClickPackets(true);
+        }
+
+        boolean cancel = this.onKeyActionImpl(action, key);
+
+        ClickPacketBuffer.setShouldBufferClickPackets(false);
+
+        return cancel;
+    }
+
+    private boolean onKeyActionImpl(KeyAction action, IKeybind key)
     {
         MinecraftClient mc = MinecraftClient.getInstance();
 
@@ -167,10 +182,19 @@ public class KeybindCallbacks implements IHotkeyCallback, IClientTickHandler
     @Override
     public void onClientTick(MinecraftClient mc)
     {
-        if (this.disabled == false &&
-            mc != null &&
-            mc.player != null &&
-            GuiUtils.getCurrentScreen() instanceof HandledScreen &&
+        if (this.disabled || mc.player == null)
+        {
+            return;
+        }
+
+        ClickPacketBuffer.sendBufferedPackets(Configs.Generic.PACKET_RATE_LIMIT.getIntegerValue());
+
+        if (ClickPacketBuffer.shouldCancelWindowClicks())
+        {
+            return;
+        }
+
+        if (GuiUtils.getCurrentScreen() instanceof HandledScreen &&
             (GuiUtils.getCurrentScreen() instanceof CreativeInventoryScreen) == false &&
             Configs.GUI_BLACKLIST.contains(GuiUtils.getCurrentScreen().getClass().getName()) == false &&
             Hotkeys.KEY_MASS_CRAFT.getKeybind().isKeybindHeld())
@@ -181,6 +205,11 @@ public class KeybindCallbacks implements IHotkeyCallback, IClientTickHandler
 
             if (outputSlot != null)
             {
+                if (Configs.Generic.RATE_LIMIT_CLICK_PACKETS.getBooleanValue())
+                {
+                    ClickPacketBuffer.setShouldBufferClickPackets(true);
+                }
+
                 RecipePattern recipe = RecipeStorage.getInstance().getSelectedRecipe();
 
                 InventoryUtils.tryClearCursor(gui, mc);
@@ -205,6 +234,8 @@ public class KeybindCallbacks implements IHotkeyCallback, IClientTickHandler
                     InventoryUtils.throwAllNonRecipeItemsToGround(recipe, gui);
                     InventoryUtils.tryMoveItemsToFirstCraftingGrid(recipe, gui, true);
                 }
+
+                ClickPacketBuffer.setShouldBufferClickPackets(false);
             }
         }
     }