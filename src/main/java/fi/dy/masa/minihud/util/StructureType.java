28fa0b57f9e94e68a0634b1b4806a1315d2e4203
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2020-08-13 01:36:15 +0300

    Fix the Structure Bounding Boxes not working on servers

--- src/main/java/fi/dy/masa/minihud/util/StructureType.java
+++ src/main/java/fi/dy/masa/minihud/util/StructureType.java
@@ -1,44 +1,39 @@
 package fi.dy.masa.minihud.util;
 
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Locale;
-import java.util.Optional;
 import javax.annotation.Nullable;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
-import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.dimension.DimensionType;
 import net.minecraft.world.gen.feature.StructureFeature;
 import fi.dy.masa.minihud.config.StructureToggle;
 
 public enum StructureType
 {
-    BURIED_TREASURE     (StructureToggle.OVERLAY_STRUCTURE_BURIED_TREASURE,     "buried_treasure",      Collections.singletonList(DimensionType.OVERWORLD_REGISTRY_KEY)),
-    DESERT_PYRAMID      (StructureToggle.OVERLAY_STRUCTURE_DESERT_PYRAMID,      "desert_pyramid",       Collections.singletonList(DimensionType.OVERWORLD_REGISTRY_KEY)),
-    IGLOO               (StructureToggle.OVERLAY_STRUCTURE_IGLOO,               "igloo",                Collections.singletonList(DimensionType.OVERWORLD_REGISTRY_KEY)),
-    JUNGLE_TEMPLE       (StructureToggle.OVERLAY_STRUCTURE_JUNGLE_TEMPLE,       "jungle_pyramid",       Collections.singletonList(DimensionType.OVERWORLD_REGISTRY_KEY)),
-    MANSION             (StructureToggle.OVERLAY_STRUCTURE_MANSION,             "mansion",              Collections.singletonList(DimensionType.OVERWORLD_REGISTRY_KEY)),
-    MINESHAFT           (StructureToggle.OVERLAY_STRUCTURE_MINESHAFT,           "mineshaft",            Collections.singletonList(DimensionType.OVERWORLD_REGISTRY_KEY)),
-    OCEAN_MONUMENT      (StructureToggle.OVERLAY_STRUCTURE_OCEAN_MONUMENT,      "monument",             Collections.singletonList(DimensionType.OVERWORLD_REGISTRY_KEY)),
-    OCEAN_RUIN          (StructureToggle.OVERLAY_STRUCTURE_OCEAN_RUIN,          "ocean_ruin",           Collections.singletonList(DimensionType.OVERWORLD_REGISTRY_KEY)),
-    PILLAGER_OUTPOST    (StructureToggle.OVERLAY_STRUCTURE_PILLAGER_OUTPOST,    "pillager_outpost",     Collections.singletonList(DimensionType.OVERWORLD_REGISTRY_KEY)),
-    SHIPWRECK           (StructureToggle.OVERLAY_STRUCTURE_SHIPWRECK,           "shipwreck",            Collections.singletonList(DimensionType.OVERWORLD_REGISTRY_KEY)),
-    STRONGHOLD          (StructureToggle.OVERLAY_STRUCTURE_STRONGHOLD,          "stronghold",           Collections.singletonList(DimensionType.OVERWORLD_REGISTRY_KEY)),
-    VILLAGE             (StructureToggle.OVERLAY_STRUCTURE_VILLAGE,             "village",              Collections.singletonList(DimensionType.OVERWORLD_REGISTRY_KEY)),
-    WITCH_HUT           (StructureToggle.OVERLAY_STRUCTURE_WITCH_HUT,           "swamp_hut",            Collections.singletonList(DimensionType.OVERWORLD_REGISTRY_KEY)),
+    BURIED_TREASURE     (StructureToggle.OVERLAY_STRUCTURE_BURIED_TREASURE,     "buried_treasure",      DimensionType.OVERWORLD_ID),
+    DESERT_PYRAMID      (StructureToggle.OVERLAY_STRUCTURE_DESERT_PYRAMID,      "desert_pyramid",       DimensionType.OVERWORLD_ID),
+    IGLOO               (StructureToggle.OVERLAY_STRUCTURE_IGLOO,               "igloo",                DimensionType.OVERWORLD_ID),
+    JUNGLE_TEMPLE       (StructureToggle.OVERLAY_STRUCTURE_JUNGLE_TEMPLE,       "jungle_pyramid",       DimensionType.OVERWORLD_ID),
+    MANSION             (StructureToggle.OVERLAY_STRUCTURE_MANSION,             "mansion",              DimensionType.OVERWORLD_ID),
+    MINESHAFT           (StructureToggle.OVERLAY_STRUCTURE_MINESHAFT,           "mineshaft",            DimensionType.OVERWORLD_ID),
+    OCEAN_MONUMENT      (StructureToggle.OVERLAY_STRUCTURE_OCEAN_MONUMENT,      "monument",             DimensionType.OVERWORLD_ID),
+    OCEAN_RUIN          (StructureToggle.OVERLAY_STRUCTURE_OCEAN_RUIN,          "ocean_ruin",           DimensionType.OVERWORLD_ID),
+    PILLAGER_OUTPOST    (StructureToggle.OVERLAY_STRUCTURE_PILLAGER_OUTPOST,    "pillager_outpost",     DimensionType.OVERWORLD_ID),
+    SHIPWRECK           (StructureToggle.OVERLAY_STRUCTURE_SHIPWRECK,           "shipwreck",            DimensionType.OVERWORLD_ID),
+    STRONGHOLD          (StructureToggle.OVERLAY_STRUCTURE_STRONGHOLD,          "stronghold",           DimensionType.OVERWORLD_ID),
+    VILLAGE             (StructureToggle.OVERLAY_STRUCTURE_VILLAGE,             "village",              DimensionType.OVERWORLD_ID),
+    WITCH_HUT           (StructureToggle.OVERLAY_STRUCTURE_WITCH_HUT,           "swamp_hut",            DimensionType.OVERWORLD_ID),
 
-    RUINED_PORTAL       (StructureToggle.OVERLAY_STRUCTURE_RUINED_PORTAL,       "ruined_portal",        ImmutableList.of(DimensionType.OVERWORLD_REGISTRY_KEY, DimensionType.THE_NETHER_REGISTRY_KEY)),
+    RUINED_PORTAL       (StructureToggle.OVERLAY_STRUCTURE_RUINED_PORTAL,       "ruined_portal",        DimensionType.OVERWORLD_ID, DimensionType.THE_NETHER_ID),
 
-    BASTION_REMNANT     (StructureToggle.OVERLAY_STRUCTURE_BASTION_REMNANT,     "bastion_remnant",      Collections.singletonList(DimensionType.THE_NETHER_REGISTRY_KEY)),
-    NETHER_FOSSIL       (StructureToggle.OVERLAY_STRUCTURE_NETHER_FOSSIL,       "nether_fossil",        Collections.singletonList(DimensionType.THE_NETHER_REGISTRY_KEY)),
-    NETHER_FORTRESS     (StructureToggle.OVERLAY_STRUCTURE_NETHER_FORTRESS,     "fortress",             Collections.singletonList(DimensionType.THE_NETHER_REGISTRY_KEY)),
+    BASTION_REMNANT     (StructureToggle.OVERLAY_STRUCTURE_BASTION_REMNANT,     "bastion_remnant",      DimensionType.THE_NETHER_ID),
+    NETHER_FOSSIL       (StructureToggle.OVERLAY_STRUCTURE_NETHER_FOSSIL,       "nether_fossil",        DimensionType.THE_NETHER_ID),
+    NETHER_FORTRESS     (StructureToggle.OVERLAY_STRUCTURE_NETHER_FORTRESS,     "fortress",             DimensionType.THE_NETHER_ID),
 
-    END_CITY            (StructureToggle.OVERLAY_STRUCTURE_END_CITY,            "endcity",              Collections.singletonList(DimensionType.THE_END_REGISTRY_KEY));
+    END_CITY            (StructureToggle.OVERLAY_STRUCTURE_END_CITY,            "endcity",              DimensionType.THE_END_ID);
 
     public static final ImmutableList<StructureType> VALUES;
     private static final HashMap<String, StructureType> ID_TO_TYPE = new HashMap<>();
@@ -70,23 +65,19 @@ public enum StructureType
     private final StructureToggle toggle;
     private final String structureName;
     private final StructureFeature<?> feature;
-    private final ImmutableSet<RegistryKey<DimensionType>> dimKeys;
-    private final HashMap<DimensionType, Boolean> dimCache = new HashMap<>();
+    private final ImmutableSet<Identifier> dims;
 
-    StructureType(StructureToggle toggle, String structureName, List<RegistryKey<DimensionType>> dimKeys)
+    StructureType(StructureToggle toggle, String structureName, Identifier... dims)
     {
         this.toggle = toggle;
         this.structureName = structureName;
         this.feature = StructureFeature.STRUCTURES.get(structureName.toLowerCase(Locale.ROOT));
-        this.dimKeys = ImmutableSet.copyOf(dimKeys);
+        this.dims = ImmutableSet.copyOf(dims);
     }
 
     public boolean existsInDimension(DimensionType dimId)
     {
-        return this.dimCache.computeIfAbsent(dimId, (d) -> {
-            Optional<RegistryKey<DimensionType>> optional = MinecraftClient.getInstance().world.getRegistryManager().getDimensionTypes().getKey(d);
-            return optional.isPresent() && this.dimKeys.contains(optional.get());
-        });
+        return this.dims.contains(dimId.getSkyProperties()); // a bit of a meh... but works in vanilla
     }
 
     public String getStructureName()
