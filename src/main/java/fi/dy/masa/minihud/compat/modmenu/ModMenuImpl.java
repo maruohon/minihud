c7401973cdc032931e06c9b955b32e5a17a3a0cc
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2021-06-02 00:54:08 +0300

    Update Mod Menu to the new package and maven

--- src/main/java/fi/dy/masa/minihud/compat/modmenu/ModMenuImpl.java
+++ src/main/java/fi/dy/masa/minihud/compat/modmenu/ModMenuImpl.java
@@ -1,8 +1,8 @@
 package fi.dy.masa.minihud.compat.modmenu;
 
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
 import fi.dy.masa.minihud.gui.GuiConfigs;
-import io.github.prospector.modmenu.api.ConfigScreenFactory;
-import io.github.prospector.modmenu.api.ModMenuApi;
 
 public class ModMenuImpl implements ModMenuApi
 {
