ac3024d011e100114a32f3dcf8a09cc5b0d03f81
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2022-07-28 23:37:05 +0300

    Update to MC 1.19.1

--- src/main/java/fi/dy/masa/minihud/mixin/MixinChatScreen.java
+++ src/main/java/fi/dy/masa/minihud/mixin/MixinChatScreen.java
@@ -3,7 +3,7 @@ package fi.dy.masa.minihud.mixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import net.minecraft.client.gui.screen.ChatScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
@@ -17,27 +17,12 @@ public abstract class MixinChatScreen extends Screen
         super(title);
     }
 
-    @Inject(method = "sendMessage(Ljava/lang/String;Z)V", at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/client/network/ClientPlayerEntity;sendChatMessage(Ljava/lang/String;Lnet/minecraft/text/Text;)V"),
-            cancellable = true)
-    private void onSendChatMessage(String msg, boolean addToChat, CallbackInfo ci)
+    @Inject(method = "sendMessage", at = @At("HEAD"), cancellable = true)
+    private void onSendChatMessage(String msg, boolean addToHistory, CallbackInfoReturnable<Boolean> cir)
     {
         if (DataStorage.getInstance().onSendChatMessage(msg))
         {
-            ci.cancel();
-        }
-    }
-
-    @Inject(method = "sendMessage(Ljava/lang/String;Z)V", at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/client/network/ClientPlayerEntity;sendCommand(Ljava/lang/String;Lnet/minecraft/text/Text;)V"),
-            cancellable = true)
-    private void onSendCommand(String msg, boolean addToChat, CallbackInfo ci)
-    {
-        if (DataStorage.getInstance().onSendChatMessage(msg))
-        {
-            ci.cancel();
+            cir.setReturnValue(true);
         }
     }
 }
